387c1c260e6392f8018e4290e7dc56df700a187d
Kaikala Sai Sri Harish <kaikalasriharish2382@gmail.com>
2025-02-13 21:39:21 +0530

    changed_details

--- server/config.js
+++ server/config.js
@@ -1,10 +1,10 @@
 import SES from "aws-sdk/clients/ses.js";
 import S3 from "aws-sdk/clients/s3.js";
 import NodeGeocoder from "node-geocoder";
-export const DATABASE = "mongodb+srv://kaikalasriharish2382:vJFNorWxdb4COd9a@cluster0.tcpsnbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const DATABASE = "databaseid";
 // export const DATABASE = "mongodb+srv://kaikalaaa:vJFNorWxdb4COd9a@cluster0.mjpeg6x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-export const AWS_ACCESS_KEY_ID = "AKIAQZT4JRWN7LNA7WUK";
-export const AWS_SECRET_ACCESS_KEY = "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";
+export const AWS_ACCESS_KEY_ID = "accesskeyid";
+export const AWS_SECRET_ACCESS_KEY = "secret_access_key";
 export const EMAIL_FROM ="sriharishkaikala2382@gmail.com";
 export const REPLY_TO = "sriharishkaikala2382@gmail.com";
 
@@ -18,13 +18,13 @@ apiVersion: "2012-10-17",
 
 const options = {
   provider: "google",
-  apiKey: "AIzaSyC3t_dexgWfU379zR7bcLzuNUaBkS2f-_k",
+  apiKey: "",
   formatter: null,
 };
 
 export const AWSSES = new SES (awsConfig);
 export const AWSS3 = new S3(awsConfig);
-export const JWT_SECRET = "0e441230f7e36562e5505d7f387d7768ab28a94f2d832256f7bba6f3234b9cd3";
+export const JWT_SECRET = "jwt_secret";
 export const CLIENT_URL = "http://localhost:3000";
 export const GOOGLE_GEOCODER = NodeGeocoder(options);
 
